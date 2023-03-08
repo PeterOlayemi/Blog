a45b84e5cbc0390bffa104192b888f10192f87c9
PeterOlayemi <olayemipeter2005@gmail.com>
2023-03-08 13:36:16 +0100

    new commit

--- main/settings.py
+++ main/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%9$m!1ywnd_i=17)@*@l#$j6@*0uk^flksg=hor^@5nm2n%+de'
+
+from decouple import config
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
